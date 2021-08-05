ec45e3534e446beed79b3c4083e5b0e2c1dc0f1c
Kevin Nelson <kevin.nelson@mail.utoronto.ca>
2021-08-05 12:16:12 +0000

    linted all current lotide files

--- without.js
+++ without.js
@@ -1,27 +1,23 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
+const eqArrays = (resultArray, testArray) => resultArray.join() === testArray.join() ? true : false; //ternary, if the string containing all the values in array1 matches the string containing all the values in array2, this function will return the boolean true
 
-without = (originArray, removalArray) => {
+const assertArraysEqual = (resultArray, testArray) => eqArrays(resultArray, testArray) ? console.log("✅✅✅ Assertion Passed, both arrays are equal") : console.log("🛑🛑🛑 Assertion Failed, the arrays are not equal");
+
+const without = (originArray, removalArray) => {
   let resultArray = [];
   for (let i = 0; i < originArray.length; i++) {
     let findMatch = false;
-    for (let j = 0; j < removalArray.length; j++){
-        if (removalArray[j] === originArray[i]) {
-          findMatch = true;
-        }
+    for (let j = 0; j < removalArray.length; j++) {
+      if (removalArray[j] === originArray[i]) {
+        findMatch = true;
+      }
       
     }
     if (!findMatch) {
-      resultArray.push(originArray[i])
+      resultArray.push(originArray[i]);
     }
   }
-  console.log(resultArray);
+  return resultArray;
 };
 
-without([1, 2, 3], [1, 2]); // => [2, 3]
-//without(["1", "2", "3"], [1, 2, "3"]); // => ["1", "2"]
+assertArraysEqual((without([1, 2, 3], [1])), [2, 3]);
+assertArraysEqual(without(["1", "2", "3"], [1, 2, "3"]),["1", "2"]);
