6e8fdfeb2c5df2ebe51801bd2219f96a43d6562f
Kevin Nelson <kevin.nelson@mail.utoronto.ca>
2021-08-12 12:13:56 +0000

    added findKeyByValue to lotide folder

---
+++ findKeyByValue.js
@@ -0,0 +1,24 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const bestTVShowsByGenre = {
+  sci_fi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama:  "The Wire"
+};
+
+const findKeyByValue = (bestTVShowsByGenre, value) => {
+  let genre = "";
+  for (const index in bestTVShowsByGenre) {
+    value === bestTVShowsByGenre[index] ? genre += index : undefined;
+  }
+  return genre;
+};
+
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Expanse"), "sci_fi");
+
