b5c8df9b89cf5bc5ba32283d776fddf72221e8ac
Kevin Nelson <kevin.nelson@mail.utoronto.ca>
2021-08-13 00:50:53 +0000

    added assertObjectEquals.js to lotide, which checks if two objects are the same and logs a message to the console

---
+++ assertObjectsEqual.js
@@ -0,0 +1,28 @@
+const assertObjectsEqual = (actual, expected) => {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const eqObjects = (object1, object2) => {
+  let object1Key = Object.getOwnPropertyNames(object1)
+  let object2Key = Object.getOwnPropertyNames(object2);
+
+  if (object1Key.length !== object2Key.length) {
+    return false;
+  } 
+  for (let i = 0; i < object1Key.length; i++) {
+    let keyName = object1Key[i];
+    if (object1[keyName] !== object2[keyName]) {
+      return false;
+    }
+  }
+
+  return true;
+
+};
+const ab = { a: "1", b: "2" };
+const ba = { b: "2", a: "1" };
+assertObjectsEqual((eqObjects(ab, ba)), true);
